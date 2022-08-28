8f20662632c975b012c90529509a3ac6e2297699
Zachary Noble <zachnoble1993@gmail.com>
2022-08-28 15:27:56 -0400

    build dirs are parent not scripts dir

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,6 +2,6 @@
 
 # build client and service images
 
-docker build -t jcapt-client -f ../client.Dockerfile .
+docker build -t jcapt-client -f ../client.Dockerfile ..
 
-docker build -t jcapt-service -f ../service.Dockerfile .
+docker build -t jcapt-service -f ../service.Dockerfile ..
